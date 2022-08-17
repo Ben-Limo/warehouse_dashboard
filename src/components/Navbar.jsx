83f44a836abcbfae0b2e24eac99f11e3270521cd
Ben Limo <bensokiplimo@gmail.com>
2022-08-17 06:09:23 +0300

    add mobile friendly sidebar and navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import avatar from '../data/avatar.jpg';
+import avatar from '../data/avatar.png';
 import { Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -15,15 +15,31 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       style={{color}}
       className="relative text-xl rounded-full p-3 hover:bg-light-gray">
         <span style={{ background: dotColor }}
-          className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-            {icon}
-        </span>
+          className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
+        {icon}
     </button>
   </TooltipComponent>
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } =  useStateContext()
+  const { activeMenu, setActiveMenu, isClicked,
+     setIsClicked, handleClick, screenSize, setScreenSize } =  useStateContext()
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+
+    window.addEventListener('resize', handleResize)
+
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [setScreenSize])
+
+  useEffect(() => {
+    screenSize <= 900 ? 
+    setActiveMenu(false) : 
+    setActiveMenu(true)
+  }, [screenSize, setActiveMenu])
 
   return (
     <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
